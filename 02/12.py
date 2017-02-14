527134a9770a5f6e76753455dd43195bccdc5cc7
Laurel152 <chiko_leaf152@yahoo.co.jp>
2017-02-14 16:25:29 +0900

    12問目終了

---
+++ 02/12.py
@@ -0,0 +1,19 @@
+inputfile = open('hightemp.txt', 'r')
+outputfile1 = open('col1.txt', 'w')
+outputfile2 = open('col2.txt', 'w')
+
+#行数取得(最後の改行カット)
+rownums = len(inputfile.readlines())
+
+inputfile.seek(0)
+datalists = []
+for i in range(rownums):
+	buf = inputfile.readline().replace('\n','')
+	datalists.append(buf.split('\t'))
+	
+	outputfile1.write(datalists[i][0] + '\n')
+	outputfile2.write(datalists[i][1] + '\n')
+	
+inputfile.close()
+outputfile1.close()
+outputfile2.close()
